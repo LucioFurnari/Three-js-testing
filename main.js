ac8c455831184b40dd242619994bc8c36df75395
LucioFurnari <luciofurnari99@gmail.com>
2024-01-14 20:24:31 -0300

    Add plane and grid helper to the scene

--- main.js
+++ main.js
@@ -1,26 +1,41 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGL1Renderer();
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Create cube geometry and material
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-
 scene.add(cube);
 
+// Create plane
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = -0.5 * Math.PI;
+scene.add(plane);
+
+// Add grid helper
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
 camera.position.z = 5;
+orbit.update();
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
