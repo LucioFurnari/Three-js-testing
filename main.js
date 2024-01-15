e5a86819c61aa3019e13d03b44ba4b2694431858
LucioFurnari <luciofurnari99@gmail.com>
2024-01-15 17:30:04 -0300

    Add ambient and directional light, and set shadows

--- main.js
+++ main.js
@@ -6,29 +6,61 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const renderer = new THREE.WebGL1Renderer();
 
+renderer.shadowMap.enabled = true;
+
 const orbit = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Create ambient light
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+//Create directional light
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10);
+directionalLight.castShadow = true;
+directionalLight.position.set(-30, 50, 0);
+scene.add(directionalLight);
+
+// Directiona light helper
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+scene.add(dLightHelper)
+
 // Create cube geometry and material
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // Create plane
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF});
+const planeMaterial = new THREE.MeshStandardMaterial({ 
+  color: 0xFFFFFF,
+  side: THREE.DoubleSide,
+});
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
 scene.add(plane);
+
+// Create sphere
+const sphereGeometry = new THREE.SphereGeometry(4);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  color: 0x2e56db,
+  wireframe: false,
+});
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.castShadow = true;
+sphere.position.y = 10
+scene.add(sphere);
 
 // Add grid helper
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-camera.position.z = 5;
+camera.position.y = 20;
+camera.position.x = 20
 orbit.update();
 
 function animate() {
