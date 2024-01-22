6e5ce9f199ab790383368ce84e7c55785ca129b5
LucioFurnari <luciofurnari99@gmail.com>
2024-01-22 18:23:03 -0300

    Add event to make camera responsive

--- main.js
+++ main.js
@@ -170,3 +170,9 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
